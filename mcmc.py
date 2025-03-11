de4ecc97ae22b3663aa24797682770d2270e5bc9
IvanNikolic21 <ivan.nikolic@sns.it>
2025-03-11 16:45:42 +0100

    Speed up.
    Notes:
    - Found some solutions to speed-up the calculations. Mainly by reducing the spacing for r- and k-bins in power spectra calculations. Speed-up is 10x
    - adapted likelihoods and priors to analyze the speed-up.

--- mcmc.py
+++ mcmc.py
@@ -282,10 +282,12 @@ def run_mcmc(
 if __name__ == "__main__":
     #initialize likelihoods
     #likelihoods = ["Ang_z9_m87", "Ang_z9_m9", "UVLF_z11_McLeod23"]
-    likelihoods = ["UVLF_z11_McLeod23"]
-    params = ["fstar_scale", "alpha_star_low", "t_star", "sigma_SHMR", "sigma_SFMS_norm"]
-    priors = [(-1.0,1.0),(0.0,1.0), (0.05,0.9), (0.01,1.0), (0.01,1.0)]
-
+    likelihoods = ["Ang_z9_m87"]
+    #likelihoods = ["UVLF_z11_McLeod23"]
+    #params = ["fstar_scale", "alpha_star_low", "t_star", "sigma_SHMR", "sigma_SFMS_norm"]
+    params = ["fstar_scale", "sigma_SHMR", "alpha_star_low"]
+    #priors = [(-1.0,1.0),(0.0,1.0), (0.05,0.9), (0.01,1.0), (0.01,1.0)]
+    priors = [(-1.0,1.0),(0.01,1.0), (0.0,1.0)]
     #more possibilities: "M_1", "M_0", "alpha" -> relating to satellite params.
     #new possibility: "a_sig_SFR" -> relating to sigma_SFMS scaling with stellar mass.
     #"write a list of all possible parameters"
