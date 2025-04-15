c7867c107557f6204cecf53c613f52f6a8d05ba6
IvanNikolic21 <ivan.nikolic@sns.it>
2025-04-15 16:15:32 +0200

    Adding observation of angular z=7.
    Notes:
    - I've set up everything to analyze w(theta) for other redshift.
    - analysis of all the data we have.

--- mcmc.py
+++ mcmc.py
@@ -261,7 +261,7 @@ def run_mcmc(
     if priors is None:
         priors = [(-3.0,1.0),(0.0,1.0), (0.05,0.9), (0.01,1.0), (0.01,1.0)]
     #initialize likelihoods
-    output_filename = "/home/inikolic/projects/UVLF_FMs/run_speed/runs_140425/ang_also_z7/"
+    output_filename = "/home/inikolic/projects/UVLF_FMs/run_speed/runs_140425/all_constraints/"
     #if initialized
     mult_params_fid = {
         "use_MPI": True,
@@ -489,7 +489,7 @@ if __name__ == "__main__":
     #likelihoods = ["UVLF_z11_McLeod23", "UVLF_z9_Donnan24", "UVLF_z10_Donnan24","UVLF_z11_Donnan24","UVLF_z12_5_Donnan24"]
     #likelihoods = []
     #likelihoods = ["UVLF_z11_McLeod23"]
-    likelihoods = ["Ang_z9_m9", "Ang_z7_m9"]
+    likelihoods = ["Ang_z9_m9", "Ang_z7_m9", "UVLF_z11_McLeod23", "UVLF_z9_Donnan24", "UVLF_z10_Donnan24","UVLF_z11_Donnan24","UVLF_z12_5_Donnan24"]
     params = ["fstar_norm", "sigma_SHMR", "t_star", "alpha_star_low",
               "sigma_SFMS_norm", "a_sig_SFR"]
     priors = [(-3.0, 1.0), (0.001, 2.0), (0.001, 1.0), (0.0, 2.0), (0.001, 1.2),
