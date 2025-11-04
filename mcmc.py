47e392d4fcc17aa098070400d3748313f3893a60
IvanNikolic21 <ivan.nikolic@sns.it>
2025-11-04 16:35:33 +0100

    All other files should now be compatible

--- mcmc.py
+++ mcmc.py
@@ -682,6 +682,7 @@ def run_mcmc(
 
 
     if uvlf and use_BPASS:
+        print(script_dir)
         if script_dir == "/groups/astro/ivannik/programs/JWST-Inference":
             bpass_read = bpass_loader(
                 filename='/groups/astro/ivannik/programs/Stochasticity_sampler/BPASS/spectra-bin-imf135_300.a+00.',
